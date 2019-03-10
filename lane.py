bd3f3cfc63dc920cfde89e0d352d158311cb6bb0
prasanganath <sandun.wed@gmail.com>
2019-03-10 12:07:27 +0530

    slope

--- lane.py
+++ lane.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def canny(image):
-    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     canny = cv2.Canny(blur,50, 150)
     return canny
@@ -28,9 +28,10 @@ def region_of_interest(image):
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-canny = canny(lane_image)
-cropped_image = region_of_interest(canny)
+cannyi_image = canny(lane_image)
+cropped_image = region_of_interest(cannyi_image)
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+averaged_lines = average_slope_intercept(lane_image, lines)
 line_image = display_lines(lane_image, lines)
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 cv2.imshow('result', combo_image)
