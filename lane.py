d600c4b5c88f9e221416568c83e08bc44240b1e9
prasanganath <sandun.wed@gmail.com>
2019-03-10 09:57:37 +0530

    find values

--- lane.py
+++ lane.py
@@ -8,6 +8,13 @@ def canny(image):
     canny = cv2.Canny(blur,50, 150)
     return canny
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            print(line)
+
+
 def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([[(200, height ), (1100, height), (550, 250)]])
@@ -21,6 +28,7 @@ image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
-
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
 cv2.imshow('result', cropped_image)
 cv2.waitKey(0)
