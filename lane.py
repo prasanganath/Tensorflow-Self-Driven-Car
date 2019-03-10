699e664f9339a468bdee1e7ca7bd3e1a54fddd61
prasanganath <sandun.wed@gmail.com>
2019-03-10 06:49:39 +0530

    polygons

--- lane.py
+++ lane.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 def canny(image):
     gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
@@ -8,10 +8,17 @@ def canny(image):
     canny = cv2.Canny(blur,50, 150)
     return canny
 
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([[(200, height ), (1100, height), (550, 250)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    return mask
+
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 
-plt.imshow(canny)
-plt.show()
+cv2.imshow('result', region_of_interest(canny))
+cv2.waitKey(0)
